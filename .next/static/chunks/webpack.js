fe8bdc3fc6c73f977909678243d54e3a91c01166
FabianMp250822 <fabianmunzpuello@hotmail.com>
2024-10-21 14:03:34 -0500

    cambios de compontes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "82940fe2ccceed14"; }
+/******/ 		__webpack_require__.h = function() { return "661f96d521b02510"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
