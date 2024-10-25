43131e732e30d275403a92d26bed907c818dc6b7
FabianMp250822 <fabianmunzpuello@hotmail.com>
2024-10-24 21:07:11 -0500

    cambios base

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e57f4c8674e5212a")
+/******/ 		__webpack_require__.h = () => ("25c01351327eb1b0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
