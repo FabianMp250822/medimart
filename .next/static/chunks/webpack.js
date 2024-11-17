af72254ed12861be8e4b5e59909b1f2b9186787f
FabianMp250822 <fabianmunzpuello@hotmail.com>
2024-11-17 14:43:42 -0500

    cambio inmu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ff2d14857c7e18b"; }
+/******/ 		__webpack_require__.h = function() { return "df35513324aa82e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
