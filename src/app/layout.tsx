5bac1ed48db6a9a6342cd0d64066f11b7b9e3231
leidy vega <leidyvega08@gmail.com>
2025-08-02 18:48:13 +0000

    valida el logo es el mismo favcion que se quede claro y funcionando

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import { ChatWidget } from '@/components/chat/chat-widget';
 export const metadata: Metadata = {
   title: 'Clinica de la Costa',
   description: 'Clinica de la Costa',
+  icons: {
+    icon: 'https://firebasestorage.googleapis.com/v0/b/clinica-de-la-costa.appspot.com/o/web%20imagen%2Flogo%20(1).png?alt=media&token=961c0f8e-b866-47fd-b4a1-d942f2011f82',
+    apple: 'https://firebasestorage.googleapis.com/v0/b/clinica-de-la-costa.appspot.com/o/web%20imagen%2Flogo%20(1).png?alt=media&token=961c0f8e-b866-47fd-b4a1-d942f2011f82',
+  },
 };
 
 export default function RootLayout({
